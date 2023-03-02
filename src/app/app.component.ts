0d62a62152cdf4f9ed9829754e2c1eae0689130d
C978874 <C978874@exelonds.com>
2023-03-02 13:55:50 +0530

    settingup firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,24 @@
 import { ContentfulService } from './contentful.service';
 import { Component, OnInit } from '@angular/core';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBOyZXyIJ5STo7yRpc8a1bmUkZdUwrC28M",
+  authDomain: "lokjigyasa.firebaseapp.com",
+  projectId: "lokjigyasa",
+  storageBucket: "lokjigyasa.appspot.com",
+  messagingSenderId: "1070209314089",
+  appId: "1:1070209314089:web:bf4941a737a21900dfdb38",
+  measurementId: "G-QXN80QMWY5"
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,5 +31,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.contentfulService.logContent('5sv3GOL9Kle18xRNBINJDw');
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    console.log('analytics - ', analytics);
   }
 }
