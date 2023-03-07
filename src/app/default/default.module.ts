58740ac396e703ea5770ab75a99f3871bc0377a6
C978874 <C978874@exelonds.com>
2023-03-07 13:58:02 +0530

    Added default page route component

--- src/app/default/default.module.ts
+++ src/app/default/default.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+  },
+];
 
 @NgModule({
   declarations: [
     DefaultComponent
   ],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ]
 })
